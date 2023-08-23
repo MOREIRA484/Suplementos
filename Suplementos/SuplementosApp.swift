4bc8f1e8a705ace1687d00520c225e25e2690a0e
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-08-23 02:22:40 -0300

    Initial Commit

---
+++ Suplementos/SuplementosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuplementosApp.swift
+//  Suplementos
+//
+//  Created by leonardo Moreira on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SuplementosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
